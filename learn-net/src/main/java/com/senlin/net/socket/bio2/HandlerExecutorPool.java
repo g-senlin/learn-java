4ab5b4baf22f08345f663768417673e3bcbd3315
forest1014 <forest1014@sina.cn>
2019-02-12 08:45:07 +0800

    socket learn

---
+++ learn-net/src/main/java/com/senlin/net/socket/bio2/HandlerExecutorPool.java
@@ -0,0 +1,29 @@
+package com.senlin.net.socket.bio2;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author gsl
+ * @date 2019/2/10
+ */
+public class HandlerExecutorPool {
+    private ExecutorService executor;
+
+    public HandlerExecutorPool(int maxPoolSize, int queueSize) {
+        this.executor = new ThreadPoolExecutor(
+                Runtime.getRuntime().availableProcessors(),
+                maxPoolSize,
+                120L,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(queueSize));
+    }
+
+    public void execute(Runnable task) {
+        this.executor.execute(task);
+    }
+
+}
+
